355cb458ba89fa88503ba5710aead1c9e584a403
TimurPaneshKubSU <107218070+TimurPaneshKubSU@users.noreply.github.com>
2022-06-12 23:04:30 +0300

    Add files via upload

---
+++ orders/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import (
+    Service
+)
+
+admin.site.register(Service)
+
---
+++ orders/models.py
@@ -0,0 +1,20 @@
+
+from django.db import models
+from django.forms import CharField
+from salons.models import Salon
+
+class Service(models.Model):
+    """
+    name -models.CharField
+    url - models.CharField
+
+    """
+
+    name = models.CharField(verbose_name='Нименование услуги', max_length=256)
+    duration = models.CharField(verbose_name='Время оказания', max_length=256)
+    price = models.CharField(verbose_name='Цена', max_length=256)
+    salon = models.ForeignKey(Salon, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
