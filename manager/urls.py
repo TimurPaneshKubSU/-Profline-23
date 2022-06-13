8dd3052e1ab65a3a4331f630b5c258eb4cede745
TimurPaneshKubSU <107218070+TimurPaneshKubSU@users.noreply.github.com>
2022-06-13 23:47:03 +0300

    Исправление 3 - Устранение сбоев
    Поле «ФИО клиента» обязательным для заполнения

--- manager/urls.py
+++ manager/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from salons import views
 from django.conf.urls.static import static
 from django.conf import settings
-
+ 
 urlpatterns = [
     path('', views.salon_list),
     path('admin/', admin.site.urls),
